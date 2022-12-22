9adfd378d0703a02b9acf5ce9eae49881b621142
Swaran <swaranrajlaxmi@gmail.com>
2022-12-22 22:04:19 +0800

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react"
-import reactLogo from "../images/react-logo.png"
+import reactLogo from "../images/react-logo1.png"
 
 export default function Header(){
     return (
         <header>
             <nav className="nav">
-                <img src={reactLogo} alt="Logo" className="nav-icon"/>
-                <h3>React Facts</h3>
-                <h4>React Course -  Project1</h4>
+                <img src={reactLogo} alt="Logo" className="nav--icon"/>
+                <h3 className="nav--logo_text">React Facts</h3>
+                <h4 className="nav--title">React Course -  Project1</h4>
             </nav>
         </header>
     )
