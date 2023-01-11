031b1d0d61cc25932a5bbf7baf3969d55ecec357
Swaran <swaranrajlaxmi@gmail.com>
2023-01-11 16:45:37 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import React from "react"
+
+export default function App() {
+    return (
+        <main>
+            <h1 className="title">Quizzical</h1>
+            <p className="instructions">Some description if needed</p>
+        </main>
+    )
+}
