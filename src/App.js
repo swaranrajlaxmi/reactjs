f61499235a726d12910463b9154c9821113338ac
Swaran <swaranrajlaxmi@gmail.com>
2023-01-16 14:31:48 +0800

    creates starting page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@
 import React from "react"
 
 export default function App() {
+    const [questions, setQuestions] = React.useState({})
+
+    React.useEffect(() => {
+        console.log("hi")
+        async function getQuestionsFromApi() {
+            const res = await fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
+            const data = await res.json()
+            setQuestions(data.results)
+        }
+        getQuestionsFromApi()
+    }, [])
+
+    console.log(questions)
     return (
         <main>
             <h1 className="title">Quizzical</h1>
             <p className="instructions">Some description if needed</p>
+            <button className="start-quiz">Start quiz</button>
         </main>
     )
 }
